42818a72406c16cba08002d0138dcf93e19a550b
KumarSaurabh8934 <49755853+KumarSaurabh8934@users.noreply.github.com>
2020-08-24 14:20:24 +0530

    Create Majority Element.java

---
+++ Majority Element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer,Integer> mp = new HashMap<>();
+        for(int i : nums) {
+            mp.put(i,mp.getOrDefault(i,0)+1);
+        }
+        int res = 0,max = Integer.MIN_VALUE;
+        for(int i : nums) {
+            int t = mp.get(i);
+            if(t > max) {
+                res = i;
+                max = Math.max(max,t);
+            }
+        }
+        return res;
+    }
+}
